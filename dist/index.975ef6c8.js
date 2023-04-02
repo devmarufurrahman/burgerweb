e019e188dae252d4faae3cd91a4aa91c4aba3d26
marufur-rahman <mdmarufur5682@gmail.com>
2023-04-02 09:17:11 +0600

    modal update

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -559,6 +559,7 @@ function hmrAccept(bundle, id) {
 },{}],"8lqZg":[function(require,module,exports) {
 var _dataJs = require("./data.js");
 const burgerWrapper = document.querySelector(".burger-card-wrapper");
+const burgerMOdal = document.querySelector(".burger-modal");
 class App {
     constructor(){
         this._getBurgers();
@@ -597,6 +598,20 @@ class App {
         const nodes = e.target.parentNode;
         const title = nodes.children[0].innerText;
         const price = nodes.children[1].innerText;
+        burgerMOdal.classList?.remove("opacity-0");
+        burgerMOdal.classList.add("opacity-1");
+        const html = `<div
+						class="modal-container bg-gray-200 h-40 w-72 rounded-md shadow-xl z-10 overflow-hidden flex flex-col p-8 items-center duration-500 ease-in fixed left-2/4 top-2/4 -translate-x-2/4">
+						<span class="close absolute right-1 top-1 cursor-pointer">❌</span>
+						<h3 class="title-modal text-2xl pb-4 font-semibold">${title}</h3>
+						<p class="price-modal">${price}</p>
+					</div>`;
+        burgerMOdal.innerHTML = html;
+        const closeModal = document.querySelector(".close");
+        closeModal.addEventListener("click", function(e) {
+            // burgerMOdal.classList?.remove("opacity-1");
+            burgerMOdal.classList.add("opacity-0");
+        });
     }
 }
 const newApp = new App();
